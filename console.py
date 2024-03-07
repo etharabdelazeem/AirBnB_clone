1f5b4a12e565468ac309f38a3dc4f65e9118a87a
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-03-07 07:50:07 +0000

    python code style fixes

--- console.py
+++ console.py
@@ -2,20 +2,25 @@
 """Defines the HBnB console"""
 import cmd
 
+
 class CmdInterpreter(cmd.Cmd):
     """Defines the command interpreter"""
     prompt = "(hbnb) "
+
     def emptyline(self):
-      """Do nothing when receiving an empty line"""
-      pass
+        """Do nothing when receiving an empty line"""
+        pass
 
     def do_quit(self, line):
         """Exit when reciving quit"""
+        print("")
         return True
-    
+
     def do_EOF(self, line):
         """Exit when reaching EOF"""
+        print("")
         return True
+
 
 if __name__ == '__main__':
     CmdInterpreter().cmdloop()
