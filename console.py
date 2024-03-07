facd31bff926669386421a60f442a3ff261509cc
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-03-07 09:25:55 +0200

    basic command interpreter behaviour

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines the HBnB console"""
+import cmd
+
+class CmdInterpreter(cmd.Cmd):
+    """Defines the command interpreter"""
+    prompt = "(hbnb) "
+    def emptyline(self):
+      """Do nothing when receiving an empty line"""
+      pass
+
+    def do_quit(self, line):
+        """Exit when reciving quit"""
+        return True
+    
+    def do_EOF(self, line):
+        """Exit when reaching EOF"""
+        return True
+
+if __name__ == '__main__':
+    CmdInterpreter().cmdloop()
