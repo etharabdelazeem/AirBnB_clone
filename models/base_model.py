93a725e1abe338f0d992365af843b562b05034b9
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-03-08 13:43:20 +0000

    Base model +

--- models/base_model.py
+++ models/base_model.py
@@ -5,20 +5,32 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
+    """ a base class for all base models"""
+    def __init__(self, *args, **kwargs):
         """initializes the base instance"""
-
-        self.id = str(uuid.uui4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            """from models import storage"""
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            """storage.new(self)"""
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """returns a string representation of the instance"""
-        return "[] ({}) {}".format(type(self).__name__, self.id, self.__dict)
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
+        """storage.save()"""
 
     def to_dict(self):
         """returns a dictionary of the instance"""
