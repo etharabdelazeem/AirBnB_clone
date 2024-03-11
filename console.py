1d9e8e826b12f071a1159e614c7f7f61c8ec9030
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-03-11 03:03:57 +0000

    task 7

--- console.py
+++ console.py
@@ -4,12 +4,12 @@ import cmd
 import sys
 from models.base_model import BaseModel
 from models.__init__ import storage
-from models.user import User
+"""from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
-from models.review import Review
+from models.review import Review"""
 
 
 class HBNBCommand(cmd.Cmd):
@@ -17,9 +17,7 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) " if sys.__stdin__.isatty() else ''
 
     classes = {
-            'BaseModel': BaseModel """, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review"""
+            'BaseModel': BaseModel
             }
     commands = ['all', 'count', 'show', 'destroy', 'update']
     types = {
